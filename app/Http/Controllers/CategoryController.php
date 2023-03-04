ee1fc46ba379a8752a3bddc51aa71d6074144c93
Maulana <maulana.t@students.amikom.ac.id>
2023-03-05 05:50:32 +0700

    add seeder

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,11 +8,6 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('category.index');
@@ -34,22 +29,6 @@ class CategoryController extends Controller
             ->make(true);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $category = new Category();
@@ -62,12 +41,6 @@ class CategoryController extends Controller
         );
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $categories = Category::findOrFail($id);
@@ -75,24 +48,6 @@ class CategoryController extends Controller
         return response()->json($categories);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $categories = Category::findOrFail($id);
@@ -105,12 +60,6 @@ class CategoryController extends Controller
         );
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $categories = Category::findOrFail($id);
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,61 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Maulana',
-            'email' => 'maul@mail.com',
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
             'password' => bcrypt('123'),
             'role' => 'admin',
         ]);
+
+        \App\Models\Category::create([
+            'name' => 'Ice Coffee',
+        ]);
+
+        $createMultipleProducts = [
+            [
+                'category_id' => 1,
+                'code' => 'PRD-000001',
+                'name' => 'Caramel Coffee',
+                'price' => 15000,
+            ],
+            [
+                'category_id' => 1,
+                'code' => 'PRD-000002',
+                'name' => 'Vanilla Coffee',
+                'price' => 15000,
+            ],
+            [
+                'category_id' => 1,
+                'code' => 'PRD-000003',
+                'name' => 'Pandan Coffee',
+                'price' => 15000,
+            ],
+            [
+                'category_id' => 1,
+                'code' => 'PRD-000004',
+                'name' => 'Matcha Coffee',
+                'price' => 15000,
+            ],
+            [
+                'category_id' => 1,
+                'code' => 'PRD-000005',
+                'name' => 'Aren Coffee',
+                'price' => 15000,
+            ],
+            [
+                'category_id' => 1,
+                'code' => 'PRD-000006',
+                'name' => 'Moccachino Coffee',
+                'price' => 15000,
+            ],
+            [
+                'category_id' => 1,
+                'code' => 'PRD-000007',
+                'name' => 'Black Coffee',
+                'price' => 10000,
+            ]
+        ];
+
+        \App\Models\Product::insert($createMultipleProducts);
     }
 }
