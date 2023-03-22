7d79ee19ddad811c300fb255632786efec1d62ad
Maulana <maulana.t@students.amikom.ac.id>
2023-03-22 20:10:51 +0700

    debugging 🪲

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
     <div class="container-fluid d-flex flex-column p-0"><a
-            class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
+            class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="/">
             <div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-coffee"></i></div>
             <div class="sidebar-brand-text mx-3"><span>{{ config('app.name') }}</span></div>
         </a>
