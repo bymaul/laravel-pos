00549fcb0707c061eecec80e061c0e644d91b8ea
Maulana <maulanaajk@gmail.com>
2024-03-01 14:16:31 +0700

    chore: formatting

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,8 +5,8 @@
         <div class="d-sm-flex justify-content-between align-items-center mb-4">
             <h3 class="text-dark mb-0">Dashboard</h3>
             @if (Auth()->user()->role == 'admin')
-                <a class="btn btn-primary btn-sm d-none d-sm-inline-block btn-icon-split" role="button"
-                    href="{{ route('report.index') }}">
+                <a class="btn btn-primary btn-sm d-none d-sm-inline-block btn-icon-split"
+                    role="button" href="{{ route('report.index') }}">
                     <span class="icon text-white-50">
                         <i class="fas fa-download fa-sm text-white-50"></i>
                     </span>
@@ -16,26 +16,36 @@
         </div>
         <div class="row">
             @if (Auth()->user()->role == 'admin')
-                <x-dashboard.admin-info :todayRevenue="$todayRevenue" :monthRevenue="$monthRevenue" :categories="$categories" :products="$products" />
+                <x-dashboard.admin-info :todayRevenue="$todayRevenue" :monthRevenue="$monthRevenue"
+                    :categories="$categories" :products="$products" />
             @else
-                <x-dashboard.user-info :todaySales="$todaySales" :monthSales="$monthSales" :categories="$categories" :products="$products" />
+                <x-dashboard.user-info :todaySales="$todaySales" :monthSales="$monthSales"
+                    :categories="$categories" :products="$products" />
             @endif
         </div>
         @if (Auth()->user()->role == 'admin')
             <div class="row">
                 <div class="col-12 col-xl-9">
-                    <div class="card shadow mb-4">
-                        <div class="card-header d-flex justify-content-between align-items-center">
+                    <div class="card mb-4 shadow">
+                        <div
+                            class="card-header d-flex justify-content-between align-items-center">
                             <h6 class="text-primary fw-bold m-0">
-                                Pendapatan {{ indonesia_date($startDate) }} s/d {{ indonesia_date($endDate) }}
+                                Pendapatan {{ indonesia_date($startDate) }} s/d
+                                {{ indonesia_date($endDate) }}
                             </h6>
                             <div class="dropdown no-arrow">
-                                <button class="btn btn-link btn-sm dropdown-toggle" aria-expanded="false"
+                                <button
+                                    class="btn btn-link btn-sm dropdown-toggle"
+                                    aria-expanded="false"
                                     data-bs-toggle="dropdown" type="button">
-                                    <i class="fas fa-ellipsis-v text-gray-400"></i>
+                                    <i
+                                        class="fas fa-ellipsis-v text-gray-400"></i>
                                 </button>
-                                <div class="dropdown-menu shadow dropdown-menu-end animated--fade-in">
-                                    <a class="dropdown-item" href="{{ route('sale.index') }}">&nbsp;Lihat Detail</a>
+                                <div
+                                    class="dropdown-menu dropdown-menu-end animated--fade-in shadow">
+                                    <a class="dropdown-item"
+                                        href="{{ route('sale.index') }}">&nbsp;Lihat
+                                        Detail</a>
                                 </div>
                             </div>
                         </div>
@@ -48,23 +58,30 @@
                     </div>
                 </div>
                 <div class="col-12 col-xl-3">
-                    <div class="card shadow mb-4">
-                        <div class="card-header d-flex justify-content-between align-items-center">
+                    <div class="card mb-4 shadow">
+                        <div
+                            class="card-header d-flex justify-content-between align-items-center">
                             <h6 class="text-primary fw-bold m-0">
                                 Produk Terlaris
                             </h6>
                             <div class="dropdown no-arrow">
-                                <button class="btn btn-link btn-sm dropdown-toggle" aria-expanded="false"
+                                <button
+                                    class="btn btn-link btn-sm dropdown-toggle"
+                                    aria-expanded="false"
                                     data-bs-toggle="dropdown" type="button">
-                                    <i class="fas fa-ellipsis-v text-gray-400"></i>
+                                    <i
+                                        class="fas fa-ellipsis-v text-gray-400"></i>
                                 </button>
-                                <div class="dropdown-menu shadow dropdown-menu-end animated--fade-in">
-                                    <a class="dropdown-item" href="{{ route('sale.index') }}">&nbsp;Lihat Detail</a>
+                                <div
+                                    class="dropdown-menu dropdown-menu-end animated--fade-in shadow">
+                                    <a class="dropdown-item"
+                                        href="{{ route('sale.index') }}">&nbsp;Lihat
+                                        Detail</a>
                                 </div>
                             </div>
                         </div>
                         <div class="card-body">
-                            <table class="table my-0" id="dataTable">
+                            <table class="my-0 table" id="dataTable">
                                 <thead>
                                     <tr>
                                         <th>Nama</th>
@@ -75,13 +92,18 @@
                                     @if ($bestSellers->count() > 0)
                                         @foreach ($bestSellers as $bestSeller)
                                             <tr>
-                                                <td>{{ $bestSeller->products->name }}</td>
-                                                <td class="text-center">{{ $bestSeller->quantity }}</td>
+                                                <td>{{ $bestSeller->products->name }}
+                                                </td>
+                                                <td class="text-center">
+                                                    {{ $bestSeller->quantity }}
+                                                </td>
                                             </tr>
                                         @endforeach
                                     @else
                                         <tr>
-                                            <td colspan="2" class="text-center">Tidak ada data</td>
+                                            <td colspan="2"
+                                                class="text-center">Tidak ada
+                                                data</td>
                                         </tr>
                                     @endif
                             </table>
