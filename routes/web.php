bda6a20172d0ddec3a95979b0875569322b4f879
Maulana <maulanaajk@gmail.com>
2024-06-01 13:02:06 +0700

    refactor: reorganize route definitions for better readability and maintainability

--- routes/web.php
+++ routes/web.php
@@ -1,76 +1,63 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ExportController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ReportController;
-use App\Http\Controllers\SaleController;
-use App\Http\Controllers\SaleDetailController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index'])
+Route::get('/', [Controllers\DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::group(['middleware' => ['auth', 'role:admin']], function () {
-    Route::get('/category/data', [CategoryController::class, 'data'])
-        ->name('category.data');
-    Route::resource('/category', CategoryController::class);
+Route::middleware(['auth', 'role:admin'])->group(function () {
 
-    Route::get('/product/data', [ProductController::class, 'data'])
-        ->name('product.data');
-    Route::post('/product/delete-selected', [ProductController::class, 'deleteSelected'])
-        ->name('product.delete-selected');
-    Route::resource('/product', ProductController::class);
+    Route::prefix('category')->name('category.')->group(function () {
+        Route::get('data', [Controllers\CategoryController::class, 'data'])->name('data');
+        Route::resource('/', Controllers\CategoryController::class)->parameters(['' => 'category']);
+    });
 
-    Route::get('/report', [ReportController::class, 'index'])
-        ->name('report.index');
-    Route::get('/report/data/{startDate}/{endDate}', [ReportController::class, 'data'])
-        ->name('report.data');
+    Route::prefix('product')->name('product.')->group(function () {
+        Route::get('data', [Controllers\ProductController::class, 'data'])->name('data');
+        Route::post('delete-selected', [Controllers\ProductController::class, 'deleteSelected'])->name('delete-selected');
+        Route::resource('/', Controllers\ProductController::class)->parameters(['' => 'product']);
+    });
 
-    Route::get('/report/export/{startDate}/{endDate}', [ExportController::class, 'export'])
-        ->name('report.export');
+    Route::prefix('report')->name('report.')->group(function () {
+        Route::get('/', [Controllers\ReportController::class, 'index'])->name('index');
+        Route::get('data/{startDate}/{endDate}', [Controllers\ReportController::class, 'data'])->name('data');
+        Route::get('export/{startDate}/{endDate}', [Controllers\ExportController::class, 'export'])->name('export');
+    });
 
-    Route::get('/user/data', [UserController::class, 'data'])
-        ->name('user.data');
-    Route::resource('/user', UserController::class);
+    Route::prefix('user')->name('user.')->group(function () {
+        Route::get('data', [Controllers\UserController::class, 'data'])->name('data');
+        Route::resource('/', Controllers\UserController::class)->parameters(['' => 'user']);
+    });
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/transaction/new', [SaleController::class, 'create'])
-        ->name('transaction.new');
-    Route::post('/transaction/save', [SaleController::class, 'store'])
-        ->name('transaction.save');
 
-    Route::get('/transaction/{id}/data', [SaleDetailController::class, 'data'])
-        ->name('transaction.data');
-    Route::get('/transaction/loadform/{total}/{received}', [SaleDetailController::class, 'loadForm'])
-        ->name('transaction.load-form');
-    Route::resource('/transaction', SaleDetailController::class)
-        ->except(['create', 'show', 'edit']);
+    Route::prefix('transaction')->name('transaction.')->group(function () {
+        Route::get('new', [Controllers\SaleController::class, 'create'])->name('new');
+        Route::post('save', [Controllers\SaleController::class, 'store'])->name('save');
+        Route::get('{id}/data', [Controllers\SaleDetailController::class, 'data'])->name('data');
+        Route::get('loadform/{total}/{received}', [Controllers\SaleDetailController::class, 'loadForm'])->name('load-form');
+        Route::resource('/', Controllers\SaleDetailController::class)
+            ->except(['create', 'show', 'edit'])
+            ->parameters(['' => 'transaction']);
+    });
 
-    Route::get('/sale/data', [SaleController::class, 'data'])
-        ->name('sale.data');
-    Route::get('/sale', [SaleController::class, 'index'])
-        ->name('sale.index');
-    Route::get('/sale/print', [SaleController::class, 'print'])
-        ->name('sale.print');
-    Route::get('/sale/{id}', [SaleController::class, 'show'])
-        ->name('sale.show');
-    Route::get('/sale/{id}/edit', [SaleController::class, 'edit'])
-        ->name('sale.edit');
-    Route::delete('/sale/{id}', [SaleController::class, 'destroy'])
-        ->name('sale.destroy');
+    Route::prefix('sale')->name('sale.')->group(function () {
+        Route::get('data', [Controllers\SaleController::class, 'data'])->name('data');
+        Route::get('/', [Controllers\SaleController::class, 'index'])->name('index');
+        Route::get('print', [Controllers\SaleController::class, 'print'])->name('print');
+        Route::get('{id}', [Controllers\SaleController::class, 'show'])->name('show');
+        Route::get('{id}/edit', [Controllers\SaleController::class, 'edit'])->name('edit');
+        Route::delete('{id}', [Controllers\SaleController::class, 'destroy'])->name('destroy');
+    });
 
-    Route::get('/profile', [ProfileController::class, 'edit'])
-        ->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])
-        ->name('profile.update');
-    Route::post('/profile', [ProfileController::class, 'store'])
-        ->name('profile.store');
+    Route::prefix('profile')->name('profile.')->group(function () {
+        Route::get('/', [Controllers\ProfileController::class, 'edit'])->name('edit');
+        Route::patch('/', [Controllers\ProfileController::class, 'update'])->name('update');
+        Route::post('/', [Controllers\ProfileController::class, 'store'])->name('store');
+    });
 });
 
 require __DIR__ . '/auth.php';
